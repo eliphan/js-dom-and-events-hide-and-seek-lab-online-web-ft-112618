371aa74990db8f64a3abce5885cb4f06de8969e0
Elise Phan <aliceconnic@gmail.com>
2019-03-18 21:38:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ function nestedTarget() {
   return document.getElementById('nested').querySelector('div.target');
 }
 
+function increaseRankBy(n) {
 let rankOfElement = document.querySelectorAll('ul.ranked-list li');
 let increaseRankBy = ( n => { return parseInt(rankOfElement, 10) * n } );
+}
 
 function deepestChild() {
   return document.getElementById('grand-node').querySelector('div');
