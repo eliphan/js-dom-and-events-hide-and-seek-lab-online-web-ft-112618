f34adf7969e038c9d902e2579e3e2df000b06313
Elise Phan <aliceconnic@gmail.com>
2019-03-18 22:10:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,3 +16,18 @@ function deepestChild() {
 }
 
 
+const rankedLists = document.querySelectorAll( '.ranked-list' )
+      const firstList = rankedLists[ 0 ]
+      const secondList = rankedLists[ 1 ]
+
+      let children = firstList.children
+      let start = 1
+      for ( let i = 0, l = children.length; i < l; i++ ) {
+        expect( parseInt( children[ i ].innerHTML ) ).to.equal( start + i + 3 )
+      }
+
+      children = secondList.children
+      start = 12
+
+      for ( let i = 0, l = children.length; i < l; i++ ) {
+        expect( parseInt( children[ i ].innerHTML ) ).to.equal( start - i + 3 )
