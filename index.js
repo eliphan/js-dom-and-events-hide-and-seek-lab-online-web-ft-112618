b4c19be6d894ff0f1150a6dbe278a3c39e1ef95e
Elise Phan <aliceconnic@gmail.com>
2019-03-18 22:11:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ function increaseRankBy( n ) {
   const rankedLists = document.querySelectorAll( '.ranked-list' );
   const firstList = rankedLists[ 0 ]
   const secondList = rankedLists[ 1 ]
-
+  let children = firstList.children
+      let start = 1
+      for ( let i = 0, l = children.length; i < l; i++ ) {
+      }
 }
 
 function deepestChild() {
