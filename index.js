8091aa7068685f409b28aafa3d277a230614eb15
Elise Phan <aliceconnic@gmail.com>
2019-03-18 22:12:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,32 +8,11 @@ function nestedTarget() {
 
 function increaseRankBy( n ) {
   const rankedLists = document.querySelectorAll( '.ranked-list' );
-  const firstList = rankedLists[ 0 ]
-  const secondList = rankedLists[ 1 ]
-  let children = firstList.children
-      let start = 1
-      for ( let i = 0, l = children.length; i < l; i++ ) {
-        parseInt( children[ i ].innerHTML ) )
+  for ( let i = 0, l = rankedLists.length; i < l; i++ ) {
+    let children = rankedLists[ i ].children
       }
 }
 
 function deepestChild() {
   return document.querySelector( '#grand-node div div div div' );
 }
-
-
-const rankedLists = document.querySelectorAll( '.ranked-list' )
-      const firstList = rankedLists[ 0 ]
-      const secondList = rankedLists[ 1 ]
-
-      let children = firstList.children
-      let start = 1
-      for ( let i = 0, l = children.length; i < l; i++ ) {
-        expect( parseInt( children[ i ].innerHTML ) ).to.equal( start + i + 3 )
-      }
-
-      children = secondList.children
-      start = 12
-
-      for ( let i = 0, l = children.length; i < l; i++ ) {
-        expect( parseInt( children[ i ].innerHTML ) ).to.equal( start - i + 3 )
