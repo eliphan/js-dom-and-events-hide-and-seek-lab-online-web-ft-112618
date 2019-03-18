3f97681c34a53ab5793c95d147c8235e235414c3
Elise Phan <aliceconnic@gmail.com>
2019-03-18 21:21:02 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target');
+}
+
+function increaseRankBy(n) {
+  return document.querySelectorAll('ul.ranked-list li');
+}
