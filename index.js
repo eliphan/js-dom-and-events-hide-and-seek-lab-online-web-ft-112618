875dd74b19fc413341967cf919fbbf64e5e112df
Elise Phan <aliceconnic@gmail.com>
2019-03-18 22:09:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,12 +12,5 @@ return parseInt(ranksOfElements) + n;
 }
 
 function deepestChild() {
-  let node = document.getElementById('grand-node');
-  let nextNode = node.children[ 0 ]
-  while ( nextNode ) {
-    node = nextNode
-    nextNode = node.children[ 0 ]
-  }
-
-  return node
+  return document.querySelector( '#grand-node div div div div'
 }
